271cdf1932882a38c38f5aa83e0a9805e73c5b57
parth thakor <parththakor2610@gmail.com>
2023-12-08 12:36:29 +0530

    adding jpg image

---
+++ w.js
@@ -0,0 +1,72 @@
+const slides = [
+  {
+    url: "https://youtu.be/017qKvwM8yM",
+    img: "/D4poster/24hour.png",
+  },
+  {
+    url: "https://youtu.be/HAQRplnEKec",
+    img: "/D4Poster/Dashera.png",
+  },
+  {
+    url: "https://youtu.be/9oOUSsje6s8",
+    img: "/D4Poster/rubercutting.png",
+  },
+  {
+    url: "https://youtu.be/S1maHWq5Y0Q",
+    img: "/D4Poster/snake.png",
+  },
+  {
+    url: "https://youtu.be/XVw247CHffw",
+    img: "/D4Poster/roketcircle.png",
+  },
+  {
+    url: "https://youtu.be/xnkp7TtNMF0",
+    img: "/D4Poster/fuljadi.png",
+  },
+  {
+    url: "https://youtu.be/a-oEoUVyC3s",
+    img: "/D4Poster/roketdori.png",
+  },
+  {
+    url: "https://youtu.be/Iip7hH1YJ_s",
+    img: "/D4Poster/bslldcroll.png",
+  },
+  {
+    img: "/D4poster/anaronmatka.png",
+    url: "https://youtu.be/8lCudq5YbeM",
+  },
+
+  {
+    url: "https://youtu.be/mKk6Q7ov3fE",
+    img: "/D4Poster/chaklibomb.png",
+  },
+
+  {
+    url: "https://youtu.be/R2wIMTO2JRI",
+    img: "/D4Poster/flyit.png",
+  },
+
+  {
+    url: "https://youtu.be/9rgExFyxYyM",
+    img: "/D4Poster/gubbare.png",
+  },
+  {
+    url: "https://youtu.be/S2CxnBpUJNE",
+    img: "/D4Poster/mustpensele.png",
+  },
+  {
+    url: "https://youtu.be/6rqTJxO1HJE",
+    img: "/D4Poster/papertips.png",
+  },
+
+
+  {
+    url: "https://youtu.be/FJViqnKz3eA",
+    img: "/D4Poster/underground.png",
+  },
+  {
+    url: "https://youtu.be/XjaEyBYVrPQ",
+    img: "/D4Poster/underwater.png",
+  },
+];
+console.log(slides.length)
