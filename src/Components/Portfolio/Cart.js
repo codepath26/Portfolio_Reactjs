4e8e2a49282a698132316a3a8ab9ea268f91bf18
parth thakor <parththakor2610@gmail.com>
2024-02-08 11:38:03 +0530

    adding the lazyload

--- src/Components/Portfolio/Cart.js
+++ src/Components/Portfolio/Cart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Link } from "react-router-dom";
 
@@ -10,19 +10,21 @@ function Cart({
   technologies,
   functionality,
 }) {
+  const [isLoaded , setIsLoaded] = useState(false);
   return (
     <>
       <div class="md:w-1/3 mt-3 md:m-0 p-4 w-full rounded-[10px] shadow-sd hover:shadow-xl transition-shadow duration-300 ">
         <div className="text-center text-indigo-700 mb-2 text-lg font-bold">
           {title}
         </div>
-          <div  className="rounded-[10px] ">
-            <LazyLoadImage
-              className="w-full h-full object-cover rounded-[10px]"
-              src={imgsrc}
-              alt="work1"
-            />
-          </div>
+        <div className="rounded-[10px] ">
+          <LazyLoadImage
+            className={`w-full ${isLoaded ? "blur-none" : "blur"} h-full object-cover rounded-[10px]`}
+            src={imgsrc}
+            alt="work1"
+            onLoad={()=>setIsLoaded(true)}
+          />
+        </div>
         <div class="layer  mt-2">
           <p>{description}</p>
           <p className="mt-2">
